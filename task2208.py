939caaa817b173da6c6e64d55ef33d9570277579
João Monteiro <joaopb187@hotmail.com>
2025-08-22 20:08:57 -0300

    First Task

---
+++ task2208.py
@@ -0,0 +1,28 @@
+# Declaradas as Listas
+masculino = []
+feminino = []
+
+# Receber os dados // Req. 01
+for i in range(4):
+    while True:
+        genero = str(input("Qual seu Gênero? ")).lower().strip()
+
+        if genero == "masculino":
+            masculino.append(float(input("Digite sua altura: ")))
+            break
+        elif genero == "feminino":
+            feminino.append(float(input("Digite sua altura: ")))
+            break
+        else:
+            print("Gênero Invalido")
+
+# Calculo da Media das Alturas Masculinas // Req. 02
+media = sum(masculino) / len(masculino)
+print(f"A media de altura das pessoas do gênero Masculino é {media}")
+# Maior e Menor altura dos Grupos // Req. 03
+print("A maior altura do Grupo Masculino é ", max(masculino))
+print("A menor altura do Grupo Masculino é ", min(masculino))
+print("A maior altura do Grupo Feminino é ", max(feminino))
+print("A menor altura do Grupo Feminino é ", min(feminino))
+# Numero de alturas no Grupo Feminino // Req. 04
+print(f"O numero de pessoas cadastradas no Grupo Feminino é {len(feminino)}")
